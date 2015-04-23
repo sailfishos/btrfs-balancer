1ed34d4d88c8d5d311fbaa4e32288cee21a939a5
Martin Grimme <martin.grimme@gmail.com>
2015-04-23 18:16:23 +0200

    removed dead code line

--- src/btrfs.cpp
+++ src/btrfs.cpp
@@ -275,7 +275,6 @@ void Btrfs::slotBalanceFinished(int exitCode, QProcess::ExitStatus status)
     if (exitCode == 0) {
         m_isBalanced = true;
         requestAllocation();
-        //emit balanceFinished(true);
     } else {
         emit balanceFinished(false, 0, 0);
     }
